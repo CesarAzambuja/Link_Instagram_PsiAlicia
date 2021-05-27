01afa79c6f75a50cf65e3208937295297f9c0198
César Azambuja <caaso.eng@gmail.com>
2021-05-27 20:15:37 -0300

    Primeiro Commit

---
+++ js/script.js
@@ -0,0 +1,3 @@
+function alert(){
+    swal("Informativo COVID", "Devido a pandemia de Covid-19, os atendimentos presenciais estão suspensos, assim que possivel informarei sobre o retorno nas minhas redes sociais.");
+}
